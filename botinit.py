2329db7893433aea2c527b923083ec3c8ee67c8d
Pardeep Bhattal <pbhattal19@gmail.com>
2023-10-21 14:49:55 -0700

    pom_timer and voicechat join functionality

--- botinit.py
+++ botinit.py
@@ -1,14 +1,18 @@
+import asyncio
 import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+from pydub import AudioSegment
+
+import pygame
 # Create an instance of the bot
 # bot = commands.Bot(command_prefix="!")  # Change the prefix as needed
 # Specify intents
 intents = discord.Intents.all()  # This requests all available events
 # If you only need certain events, you can modify this. For example:
 # intents = discord.Intents(messages=True, guilds=True) 
-
+audioSong = "audio\\Frog.mp3"
 # Create an instance of the bot with intents
 bot = commands.Bot(command_prefix="!", intents=intents)
 load_dotenv()
@@ -39,6 +43,35 @@ async def greet_all(ctx):
     await ctx.send("All members greeted!")
 
 @bot.command()
+async def play(ctx, filename):
+    # Check if the user is in a voice channel
+    if ctx.author.voice and ctx.author.voice.channel:
+        voice_channel = ctx.author.voice.channel
+        voice_client = await voice_channel.connect()
+
+        try:
+            audio = AudioSegment.from_mp3(filename)  # Load the MP3 file
+            audio.export("temp.wav", format="wav")  # Convert to WAV
+
+            voice_client.play(discord.FFmpegPCMAudio("temp.wav"))
+
+            while voice_client.is_playing():
+                await asyncio.sleep(1)
+
+            await voice_client.disconnect()
+        except Exception as e:
+            await ctx.send(f"An error occurred: {str(e)}")
+    else:
+        await ctx.send("You need to be in a voice channel to use this command.")
+
+@bot.command()
+async def join(ctx):
+    if ctx.author.voice and ctx.author.voice.channel:
+        voice_channel = ctx.author.voice.channel
+        voice_client = await voice_channel.connect()
+        await ctx.send(f'Joined {voice_channel}')
+        
+@bot.command()
 async def hello(ctx):
     await ctx.send("Hello, I'm your bot!")
 
@@ -46,10 +79,9 @@ async def hello(ctx):
 async def ping(ctx):
     await ctx.send("Pong!")
 
-
 ### Voice Channel Logic ###
 @bot.command()
-async def voice_command(ctx):
+async def pom_event(ctx):
     # Check if the command author is in a voice channel
     author_voice_state = ctx.author.voice
 
@@ -67,6 +99,15 @@ async def voice_command(ctx):
         voice_channel = author_voice_state.channel
         # You can add your logic here to join the voice channel and run a command
         await ctx.send(f"You are in {voice_channel.name} voice channel. I can run a command here.")
+        # bot join
+        if ctx.author.voice and ctx.author.voice.channel:
+            voice_channel = ctx.author.voice.channel
+            voice_client = await voice_channel.connect()
+            await ctx.send(f'Joined {voice_channel}')
+        
+@bot.command()
+async def pom_timer(ctx, minutes):
+    await ctx.invoke(bot.get_command("pom_event"))
 
 token = os.getenv('DISCORD_TOKEN')
 bot.run(token)
