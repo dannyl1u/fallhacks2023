0c6589380125a04a9fab86de600b1e57331f79c6
dannyl1u <dannyjialiliu@gmail.com>
2023-10-21 12:45:10 -0700

    update intent

--- botinit.py
+++ botinit.py
@@ -2,7 +2,14 @@ import discord
 from discord.ext import commands
 
 # Create an instance of the bot
-bot = commands.Bot(command_prefix="!")  # Change the prefix as needed
+# bot = commands.Bot(command_prefix="!")  # Change the prefix as needed
+# Specify intents
+intents = discord.Intents.all()  # This requests all available events
+# If you only need certain events, you can modify this. For example:
+# intents = discord.Intents(messages=True, guilds=True) 
+
+# Create an instance of the bot with intents
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -13,5 +20,5 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("Hello, I'm your bot!")
 
-# Replace 'YOUR_TOKEN' with the actual bot token you copied from the Discord Developer Portal
-bot.run('YOUR_TOKEN')
+token = 'MTE2NTM1MzE4ODU4MTQ0MTYwNg.G2fW0a.RkfgOBvwUU-x-cIRCQxaPkImmi7q8ivMAt32CM'
+bot.run(token)
